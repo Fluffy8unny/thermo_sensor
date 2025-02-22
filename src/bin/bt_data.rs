dd6fe28f3d5fa956651b2e33a698325d934e7ba3
Fluffy8unny <fllfybnny@gmail.com>
2025-02-22 01:17:08 +0100

    some comments, catching errors when the bt data can't be parsed

--- src/bin/bt_data.rs
+++ src/bin/bt_data.rs
@@ -1,18 +1,43 @@
 use chrono::Utc;
-use thermo_sensor::{parse_config, start_bluetooth_thread, start_database_thread, Reading,DeviceName};
+use thermo_sensor::{
+    parse_config, start_bluetooth_thread, start_database_thread, DeviceName, Reading,
+};
 use tokio::sync::mpsc;
 
+///Implementation of a function that extracts temperature and huimidity from the BT response
+///
+///This is the implementation that takes the raw response of tghe bluetooth device and 
+///parses it. If you want to use a different device you'll probably have to change it.
+///For some godforsaken reason the temperature is encoded in the concatinated bytes 3 and 4
+///where the resulting uint16 represents the temperature*10 e.g. a result of 255 means a 
+///temeperature of 25.5 centigrade.
+///
+///* `data` bluetooth response, a vector of raw data
+///* `name` identifier of the device
+///
+///* returns a reading instance containting the temperature and humidity that was encoded in the
+///result. Time is set as the current timestamp, not the time of the sending....because honestly it 
+///doens't matter for our purpose.
 fn extract_temp_and_humidity(
     data: Vec<u8>,
     name: String,
 ) -> Result<Reading, Box<dyn std::error::Error>> {
+    //for some reason once in a blue moon the data is too short.
+    //I have no idea why this happens
+    //plz send help
+    if data.len() < 5 {
+        return Err(Box::from(format!("data corrupted, this happens once every few hours.\n Got {:?}. \n If you have any idea why this happens contact me please.", data)));
+    }
     let t = u16::from_le_bytes(data[3..5].try_into()?);
     let h = data[5];
     let now = Utc::now();
     Ok(Reading {
         temperature: t,
         humidity: h,
-        device_name: DeviceName{name : name.to_string(), nickname: None},
+        device_name: DeviceName {
+            name: name.to_string(),
+            nickname: None,
+        },
         time_stamp: now,
     })
 }
--- src/reading.rs
+++ src/reading.rs
@@ -1,15 +1,28 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
-use chrono::{DateTime,Utc};
 
-#[derive(Debug, Clone,Serialize)]
+///class that represents a single measurement of temerature/humidity at a given point in time
+///
+/// *`temerature` the temerature at the time of measurement
+/// *`humidity`  the humidity at the time of measurement
+/// *`device_name` somewhat unique identifier of the device...somewha
+/// *`time_stamp` timestamp when the measurement was taken
+#[derive(Debug, Clone, Serialize)]
 pub struct Reading {
+    /// the temerature at the time of measurement
     pub temperature: u16,
+    /// the humidity at the time of measurement
     pub humidity: u8,
+    /// somewhat unique identifier of the device...somewhat
     pub device_name: DeviceName,
+    /// timestamp when the measurement was taken
     pub time_stamp: DateTime<Utc>,
 }
 
-#[derive(Debug, Clone,Serialize)]
+///Simple struct that is used to link a device to a description(nickname)
+/// *`name` identifier of the device
+/// *`nickname` a description of the device
+#[derive(Debug, Clone, Serialize)]
 pub struct DeviceName {
     pub name: String,
     pub nickname: Option<String>,
